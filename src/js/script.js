8597207d7bafb08c002163aea1830affc7fb082c
Victor-KiViK-Landwerlin <150051582+Victor-KiViK-Landwerlin@users.noreply.github.com>
2024-12-11 15:37:06 +0100

    page program

--- src/js/script.js
+++ src/js/script.js
@@ -16,3 +16,38 @@ if (toggle && nav) {
     page.classList.toggle("noscroll", isClosed);
   });
 }
+
+const lb = document.querySelector("#lightbox");
+const images = document.querySelectorAll("[data-full-img]");
+
+
+images.forEach(image => {
+    image.addEventListener("click", function() {
+        const fullImg = image.dataset.fullImg;
+        const i = lb.querySelector("img");
+        i.src = fullImg;
+        lb.showModal();
+    });
+});
+
+lb.addEventListener("click", function() {
+    lb.close();
+});
+const carousel = document.querySelector(".carousel__container");
+const prevButton = document.querySelector(".carousel__button--prev");
+const nextButton = document.querySelector(".carousel__button--next");
+const premierItem = document.querySelector(".carousel__item");
+const scrollAmount = premierItem.clientWidth;
+
+// Largeur de défilement d’un item
+if (carousel) {
+  // Scroll au clic sur le bouton précédent
+  prevButton.addEventListener("click", () => {
+    carousel.scrollBy({ left: -scrollAmount, behavior: "smooth" });;
+  });
+
+  // Scroll au clic sur le bouton suivant
+  nextButton.addEventListener("click", () => {
+    carousel.scrollBy({ left: +scrollAmount, behavior: "smooth" });;
+  });
+}
