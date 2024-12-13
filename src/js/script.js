d12e5e6ff37248371162ba878f70e15d9b29608c
Victor-KiViK-Landwerlin <150051582+Victor-KiViK-Landwerlin@users.noreply.github.com>
2024-12-13 08:06:02 +0100

    commit !!!

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,26 @@
-// *** MENU ***
-
-// On cible les éléments à modifier
 const toggle = document.querySelector(".menu-btn");
 const nav = document.querySelector(".menu");
 const page = document.body;
 
-// Vérifier si les éléments existent avant d'ajouter l'événement
+
 if (toggle && nav) {
   toggle.addEventListener("click", () => {
     const isOpen = toggle.ariaExpanded === "true";
     const isClosed = !isOpen;
-    // Mise à jour des attributs ARIA pour accessibilité
+
     toggle.ariaExpanded = isClosed;
     nav.ariaHidden = isOpen;
     page.classList.toggle("noscroll", isClosed);
+  });
+}
+if (toggle && nav) {
+  toggle.addEventListener("click", () => {
+    const isOpen = toggle.ariaExpanded === "true";
+    if (isOpen) {
+      page.style.overflow = "hidden";
+    } else {
+      page.style.overflow = "";
+    }
   });
 }
 
@@ -39,9 +46,9 @@ const nextButton = document.querySelector(".carousel__button--next");
 const premierItem = document.querySelector(".carousel__item");
 const scrollAmount = premierItem.clientWidth;
 
-// Largeur de défilement d’un item
+
 if (carousel) {
-  // Scroll au clic sur le bouton précédent
+
   prevButton.addEventListener("click", () => {
     carousel.scrollBy({ left: -scrollAmount, behavior: "smooth" });;
   });
